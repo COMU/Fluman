54d59d863e473d64695c968f7349e79df68c627d
farukuzun <farukuzun@mail.com>
2013-12-03 18:45:01 +0200

    Show config files on Flume page. Related: #2

--- app/views/showFlume.blade.php
+++ app/views/showFlume.blade.php
@@ -5,4 +5,22 @@ Apache Flume
 @stop
 
 @section('content')
+
+<table class="ink-table bordered alternating hover">
+	<th>id</th>
+	<th>name</th>
+	<th>path</th>
+	<th>isCreated</th>
+	<th>delete</th>
+@foreach ($flumeConfigs as $config)
+	<tr>
+		<td>{{ $config->id }}</td>
+		<td>{{ $config->name }}</td>
+		<td>{{ $config->path }}</td>
+		<td>{{ $config->isCreated }}</td>
+		<td><a href="{{ $config->id }}">X</a></td>
+	</tr>
+@endforeach
+</table>
+
 @stop
