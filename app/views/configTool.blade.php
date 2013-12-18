3458958e0298bdd33adb3681eff7a475ad4760ae
farukuzun <farukuzun@mail.com>
2013-12-18 16:53:02 +0200

    Add mockup of config tool as layout. Closes #21

--- app/views/configTool.blade.php
+++ app/views/configTool.blade.php
@@ -10,29 +10,112 @@ Config Tool
 @stop
 
 @section('topcontent')
-<br/>
-<div class="content-right"><a href="{{ URL::to('flume/addConfig') }}"><p><span class="ink-label info">Add a config</span></p></a></div>
 @stop
 
 @section('content')
-@if ((count($flumeConfigs) > 0))
-<table class="ink-table bordered alternating hover">
-	<th>id</th>
-	<th>name</th>
-	<th>path</th>
-	<th>isCreated</th>
-	<th>delete</th>
-@foreach ($flumeConfigs as $config)
-	<tr>
-		<td>{{ $config->id }}</td>
-		<td>{{ $config->name }}</td>
-		<td>{{ $config->path }}</td>
-		<td>{{ $config->isCreated }}</td>
-		<td><a href="{{ $config->id }}">X</a></td>
-	</tr>
-@endforeach
-</table>
-@else
-<p>You don't have any config files.</p>
-@endif
+
+<div class="ink-grid">
+    <div class="column-group gutters">
+        <div class="large-33 medium-30 small-100">
+            <div class="ink-dropdown green large-100" style="height: 300px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+                    <li class="heading active">Sources</li>
+                    <li class="separator-above active"><a href="#">Exec</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                </ul>
+                <br>
+            </div>
+
+        </div>
+        <div class="large-33 medium-30 small-100">
+            <div class="ink-dropdown blue large-100" style="height: 300px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+                    <li class="heading active">Channels</li>
+                    <li class="separator-above active"><a href="#">Memory</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                </ul>
+                <br>
+            </div>
+
+        </div>
+        <div class="large-33 medium-30 small-100">
+            <div class="ink-dropdown red large-100" style="height: 300px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+                    <li class="heading active">Sinks</li>
+                    <li class="separator-above active"><a href="#">File Roll</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                    <li class="separator-above"><a href="#">Option</a></li>
+                </ul>
+                <br>
+            </div>
+
+        </div>
+
+        <br>
+
+    </div>
+    <hr>
+
+
+    <div class="column-group gutters">
+        {{ Form::open(array('route' => 'configTool', 'class' => 'ink-form')) }}
+        <div class="large-95">
+            <div class="control-group large-50 medium-50 small-50">
+                <div class="control">
+                    {{ Form::text('configName', '', array('placeholder' => 'Provide a name to your config.')) }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
+<div class="content-right">
+<h5>Showcase</h5>
+</div>
+
+<div class="box">
+
+            <div class="large-100">
+
+    <div class="button-group">
+        <div class="content-center">
+        <button class="ink-button green">source</button>-
+        <button class="ink-button blue">channel</button>-
+        <button class="ink-button red">sink</button>
+    </div>
+    </div>
+
+            </div>
+
+</div>
+<hr>
+<div class="large-100">
+    <div class="column-group gutters">
+<div class="control append-button content-center">
+    <button class="ink-button"><i class="icon-save"></i> Save</button>
+    <button class="ink-button"><i class="icon-refresh"></i> Reset</button>
+</div>
+</div>
+</div>
+
+@stop
+
+@section('footer_scripts')
 @stop
