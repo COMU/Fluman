ef9cd8f672028d54fc3fdf3e47c9f2f265a6c14b
farukuzun <farukuzun@mail.com>
2014-01-08 01:08:28 +0200

    Show active elements on list. Related: #2

--- app/views/configTool.blade.php
+++ app/views/configTool.blade.php
@@ -17,94 +17,60 @@ Config Tool
 <div class="ink-grid">
     <div class="column-group gutters">
         <div class="large-33 medium-30 small-100">
-            <div class="ink-dropdown green large-100" style="height: 300px; overflow-y: scroll;">
-                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+            <div class="ink-dropdown green large-100" style="height: 250px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="sources">
                     <li class="heading active">Sources</li>
-                    <li class="separator-above active"><a href="#">Exec</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
                 </ul>
                 <br>
             </div>
 
         </div>
         <div class="large-33 medium-30 small-100">
-            <div class="ink-dropdown blue large-100" style="height: 300px; overflow-y: scroll;">
-                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+            <div class="ink-dropdown blue large-100" style="height: 250px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="channels">
                     <li class="heading active">Channels</li>
-                    <li class="separator-above active"><a href="#">Memory</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
                 </ul>
                 <br>
             </div>
 
         </div>
         <div class="large-33 medium-30 small-100">
-            <div class="ink-dropdown red large-100" style="height: 300px; overflow-y: scroll;">
-                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="">
+            <div class="ink-dropdown red large-100" style="height: 250px; overflow-y: scroll;">
+                <ul style="display: block; position: static;" class="dropdown-menu dropdown1" id="sinks">
                     <li class="heading active">Sinks</li>
-                    <li class="separator-above active"><a href="#">File Roll</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
-                    <li class="separator-above"><a href="#">Option</a></li>
                 </ul>
-                <br>
             </div>
-
         </div>
 
-        <br>
 
     </div>
     <hr>
 
 
-    <div class="column-group gutters">
-        {{ Form::open(array('route' => 'configTool', 'class' => 'ink-form')) }}
-        <div class="large-95">
-            <div class="control-group large-50 medium-50 small-50">
-                <div class="control">
-                    {{ Form::text('configName', '', array('placeholder' => 'Provide a name to your config.')) }}
-                </div>
-            </div>
-        </div>
-    </div>
-
 </div>
 
 <div class="box">
-
+	    <div class="column-group gutters">
+				<div class="large-20"><b>Showcase</b></div>
+                <div class="large-80 content-right">
+					{{ Form::open(array('route' => 'configTool', 'class' => 'ink-form')) }}
+                    {{ Form::text('configName', '', array('placeholder' => 'Name')) }}
+                    {{ Form::close() }}
+                </div>
             <div class="large-100">
-
     <div class="button-group">
-        <div class="content-center">
-        <button class="ink-button green">source</button>-
-        <button class="ink-button blue">channel</button>-
-        <button class="ink-button red">sink</button>
+        <div class="content-left">
     </div>
     </div>
 
             </div>
 
 </div>
-<hr>
 <div class="large-100">
     <div class="column-group gutters">
-<div class="control append-button content-center">
-    <button class="ink-button"><i class="icon-save"></i> Save</button>
-    <button class="ink-button"><i class="icon-refresh"></i> Reset</button>
+<div class="control append-button content-right">
+    <button class="ink-button"><i class="icon-save"></i></button>
+    <button class="ink-button"><i class="icon-refresh"></i></button>
 </div>
 </div>
 </div>
@@ -112,4 +78,40 @@ Config Tool
 @stop
 
 @section('footer_scripts')
+{{ HTML::script('js/all.json') }}
+<script>
+
+boxes = ["sources","channels","sinks"]
+for (var i=0;i<boxes.length;i++) {
+	box = document.getElementById(boxes[i])
+	for(var key in all[boxes[i]]) {
+      	var liElement = "<li onClick='chooseItem(\""+key+"\")' class=\"separator-above\" id=\""+key+"\"><a>"+key+" </a></li>";
+      	box.insertAdjacentHTML("beforeend",liElement);
+    }
+}
+
+function chooseItem(itemID){
+    var generalItem = document.getElementById(itemID);
+    var parentClass = generalItem.parentNode
+    var parentsChildren = parentClass.children
+
+    if (parentClass.id == "sinks" || parentClass.id == "channels"){
+        for (var j=1;j<parentsChildren.length;j++ ) {
+            parentsChildren[j].className = "separator-above"
+        }
+    }
+
+    if (generalItem.className == "separator-above") {
+        generalItem.className = "separator-above active";
+    }
+    else{
+        generalItem.className = "separator-above";
+    }
+}
+
+function addToShowCase(itemID){
+
+}
+
+</script>
 @stop
